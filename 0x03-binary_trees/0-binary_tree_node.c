4ae9b6e11b33311061bec7a24f8cfa5ea6973b95
sravanthisinha <sravani.sinha@gmail.com>
2017-07-19 16:14:07 -0700

    finish task 22

--- 0x03-binary_trees/0-binary_tree_node.c
+++ 0x03-binary_trees/0-binary_tree_node.c
@@ -1,14 +1,14 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node -  a function that creates a binary tree node
- * @parent : a pointer to the parent node of the node to create
- * @value: the value to put in the new node
- * Return: a pointer to the new node, or NULL on failure
- */
+* binary_tree_node - creates a binary tree node
+* @parent: pointer to root of tree
+* @value: value of the node
+* Return: pointer to new node or NULL
+**/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *temp, *node;
+	binary_tree_t *temp;
 
 	temp = malloc(sizeof(binary_tree_t));
 	if (!temp)
@@ -16,19 +16,6 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	temp->n = value;
 	temp->left = NULL;
 	temp->right = NULL;
-	if (parent == NULL)
-	{
-		temp->parent = NULL;
-		parent = temp;
-	}
-	else
-	{
-		node = parent;
-		if (node->n < value)
-			node->right = temp;
-		else
-			node->left = temp;
-		temp->parent = node;
-	}
+	temp->parent = parent;
 	return (temp);
 }
---
+++ 0x03-binary_trees/103-binary_tree_rotate_left.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/* resource: http://cs.lmu.edu/~ray/notes/binarysearchtrees/ */
+/**
+* binary_tree_rotate_left -  a function that performs a left-rotation
+* on a binary tree
+* @tree: a pointer to the root node of the tree to rotate
+* Return: a pointer to the new root node of the tree once rotated
+**/
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
+{
+	binary_tree_t *node, *node2, *node3;
+
+	if (!tree)
+		return (NULL);
+	if (tree->right)
+	{
+		node = tree;
+		node2 = tree->right;
+		node3 = tree->right->left;
+		node2->parent = node->parent;
+		node2->left = node;
+		node->parent = node2;
+		node->right = node3;
+		if (node3)
+			node3->parent = node;
+		tree = node2;
+	}
+	return (tree);
+}
