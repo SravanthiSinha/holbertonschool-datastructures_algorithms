149a15f3ac13768d9a613452f2091ca48c3474ff
sravanthisinha <sravani.sinha@gmail.com>
2017-09-29 21:23:16 -0700

    indluded symbol cmp

--- 0x04-huffman_coding/huffman_priority_queue.c
+++ 0x04-huffman_coding/huffman_priority_queue.c
@@ -2,6 +2,26 @@
 #include "heap.h"
 
 /**
+ * symbol_cmp - Compares two symbols
+ *
+ * @p1: First pointer
+ * @p2: Second pointer
+ *
+ * Return: Difference between the two symbols
+ */
+int symbol_cmp(void *p1, void *p2)
+{
+	binary_tree_node_t *n1, *n2;
+	int c1, c2;
+
+	n1 = (binary_tree_node_t *) p1;
+	n2 = (binary_tree_node_t *) p2;
+	c1 = ((symbol_t *) n1->data)->freq;
+	c2 = ((symbol_t *) n2->data)->freq;
+	return (c1 - c2);
+}
+
+/**
  * huffman_priority_queue -  A function that creates a priority queue for the
  * Huffman coding algorithm
  * @data: An array of characters of size `size`
@@ -17,8 +37,8 @@ heap_t *huffman_priority_queue(char *data, size_t *freq, size_t size)
 	symbol_t *symbol;
 	binary_tree_node_t *node, *nested;
 
-	priority_queue_heap = heap_create(NULL);
-	if (priority_queue_heap == NULL)
+	priority_queue_heap = heap_create(symbol_cmp);
+	if (priority_queue_heap == NULL || data == NULL || freq == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
 	{
