347e70a1d15068edb86b39570152f6020c8125ea
sravanthisinha <sravani.sinha@gmail.com>
2017-09-29 21:16:34 -0700

    task 8

--- 0x04-huffman_coding/heap/huffman.h
+++ 0x04-huffman_coding/heap/huffman.h
@@ -19,4 +19,5 @@ typedef struct symbol_s
 symbol_t *symbol_create(char data, size_t freq);
 
 heap_t *huffman_priority_queue(char *data, size_t *freq, size_t size);
+int huffman_extract_and_insert(heap_t *priority_queue);
 #endif
---
+++ 0x04-huffman_coding/huffman_extract_and_insert.c
@@ -0,0 +1,37 @@
+#include "heap.h"
+#include "huffman.h"
+/**
+ * huffman_extract_and_insert- A function that extracts the two nodes of * the priority queue and insert a new one
+ * @priority_queue: A pointer to the priority queue to extract from
+ * Return: 1 on success or 0 on failure
+ */
+int huffman_extract_and_insert(heap_t *priority_queue)
+{
+	binary_tree_node_t *first_node = NULL, *second_node = NULL;
+	size_t freq = 0;
+	symbol_t *symbol;
+	binary_tree_node_t *node, *nested;
+
+	if (priority_queue == NULL)
+		return (1);
+
+	first_node = (binary_tree_node_t *) heap_extract(priority_queue);
+	if (priority_queue->size > 0)
+		second_node =
+		    (binary_tree_node_t *) heap_extract(priority_queue);
+	if (first_node == NULL || second_node == NULL)
+		return (0);
+	freq = ((symbol_t *) first_node->data)->freq +
+	    ((symbol_t *) second_node->data)->freq;
+
+	symbol = symbol_create(-1, freq);
+	if (symbol == NULL)
+		return (0);
+	nested = binary_tree_node(NULL, symbol);
+	if (nested == NULL)
+		return (0);
+	node = heap_insert(priority_queue, nested);
+	if (node == NULL)
+		return (0);
+	return (1);
+}
